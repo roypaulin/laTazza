cee90219ee5750c9b21e58b5be0ce672ccfe3d73
ndjekoua sandjo jean thibaut <s256770@studenti.polito.it>
2019-04-30 20:46:49 +0200

    update classpath adding laTazza.gui path

--- Development/LaTazza/src/it/polito/latazza/LaTazza.java
+++ Development/LaTazza/src/it/polito/latazza/LaTazza.java
@@ -6,14 +6,14 @@ import it.polito.latazza.data.DataImpl;
 import it.polito.latazza.data.DataInterface;
 import it.polito.latazza.data.Database;
 import it.polito.latazza.exceptions.NotEnoughBalance;
-//import it.polito.latazza.gui.MainSwing;
+import it.polito.latazza.gui.MainSwing;
 
 public class LaTazza {
 	double balance=0;
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
 		DataInterface data = new DataImpl();
-		//new MainSwing(data);
+		new MainSwing(data);
        //System.console().printf("prova");
 		System.out.println("Good Test\n");
 		Database database = new Database();
