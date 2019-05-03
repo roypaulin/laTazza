6130fb53409fa1217089b2e0fd59b09dac658a01
Roy Paulin Justo Nguetsop Kenfack Djouaka <s257855@studenti.polito.it>
2019-05-03 11:57:02 +0200

    update testGetEmployeeSurname

--- Development/LaTazza/src/it/polito/latazza/test/TestDataImpl.java
+++ Development/LaTazza/src/it/polito/latazza/test/TestDataImpl.java
@@ -492,6 +492,23 @@ public class TestDataImpl {
 			System.out.println("correctly throws EmployeeException for dataImpl.getEmployeeName(-1) because id is not valid");
 		}
 	}
+    
+    @Test
+	public void testGetEmployeeSurName() throws EmployeeException {
+		dataImpl.reset();// used to clear everything before starting the Test
+		int id=-1;
+		id=dataImpl.createEmployee("john","doe");
+		//i get the beverageName of the created string
+		String empSurname = dataImpl.getEmployeeSurname(id);
+		assertEquals(empSurname,"doe");
+         
+		// throw an EmployeeException because the id is invalid
+		try {
+			empSurname=dataImpl.getEmployeeSurname(-1);
+		}catch(EmployeeException e){
+			System.out.println("correctly throws EmployeeException for dataImpl.getEmployeeSurname(-1) because id is not valid");
+		}
+	}
     @Test
     public void TestGetReport() throws Exception {
     	dataImpl.reset();
