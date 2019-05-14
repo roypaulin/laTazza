1cac2777d2bee92d03a67e5b12c9c2e4c0896d37
Elia Migliore <s267552@studenti.polito.it>
2019-05-14 13:28:08 +0200

    cleaned the method testAccountWrongAttributes inside the test

--- src/test/java/it/polito/latazza/data/TestDataImpl.java
+++ src/test/java/it/polito/latazza/data/TestDataImpl.java
@@ -536,8 +536,9 @@ public class TestDataImpl {
       //try recharge account with an invalid employee Id
         try {
         	dataImpl.rechargeAccount(-1, -1);
+        	fail();
         }catch(EmployeeException e) {
-        	System.out.println("correctly throws exception because Employee id is not valid");
+        	assert(true);
         }
       //try recharge account with a negative amount
         int credit=dataImpl.rechargeAccount(emp1, -1);
