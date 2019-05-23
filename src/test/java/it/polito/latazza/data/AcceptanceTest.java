8e99b3d23ff2987d99ea5829bf1b8bed687d5810
ndjekoua sandjo jean thibaut <s256770@studenti.polito.it>
2019-05-23 14:02:45 +0200

    modify AcceptanceTeast

--- src/test/java/it/polito/latazza/data/AcceptanceTest.java
+++ src/test/java/it/polito/latazza/data/AcceptanceTest.java
@@ -7,9 +7,9 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
+//import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+//import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
