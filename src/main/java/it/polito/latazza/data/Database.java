8dc2632fdc7289696bc5c58aabb98f4a31344397
ndjekoua sandjo jean thibaut <s256770@studenti.polito.it>
2019-05-25 17:58:43 +0200

    removed 2 useless import from Database class

--- src/main/java/it/polito/latazza/data/Database.java
+++ src/main/java/it/polito/latazza/data/Database.java
@@ -2,9 +2,6 @@
  *
  */
 package it.polito.latazza.data;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
